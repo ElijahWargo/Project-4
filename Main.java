4401296cbc5654b0a3f4d37892c038935ef5b708
ElijahWargo <Elijah.wargo@patelhs.org>
2021-02-03 19:10:12 +0000

    Added 'Scanner' so that users can put their own input

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args) 
+  {
+  //Using scanner to allow input from user
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("What is the year of your car?");
+    int year1 = scan.nextInt();
+    System.out.println("What is the make of your car?");
+    String car1 = scan.next();
+    System.out.println("What is the model of your car?");
+    String make1 = scan.next();
+    System.out.println("How many 'miles per gallon' does your car get?");
+    int mpg1 = scan.nextInt();
+
   }
 }
